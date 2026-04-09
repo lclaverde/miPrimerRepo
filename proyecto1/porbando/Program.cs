203aa3cb5f790fa96dfb5a7990848c547a4f5296
Lucho <luisclp@gmail.com>
2026-04-08 21:48:29 -0500

    nuevo gitignore creado

--- proyecto1/porbando/Program.cs
+++ proyecto1/porbando/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+
+const string configFile = "appsettings.json";
+
+if (File.Exists(configFile))
+{
+    var json = File.ReadAllText(configFile);
+    using var document = JsonDocument.Parse(json);
+    var connectionString = document.RootElement
+        .GetProperty("ConnectionStrings")
+        .GetProperty("DefaultConnection")
+        .GetString();
+
+    Console.WriteLine("Simulación de cadena de conexión:");
+    Console.WriteLine(connectionString);
+}
+else
+{
+    Console.WriteLine($"No se encontró el archivo de configuración '{configFile}'.");
+}
+
 Console.WriteLine("Hello, platzi!");
